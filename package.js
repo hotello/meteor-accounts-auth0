f0a49b698fcc571603500ec44cbce94752046b1e
Fabio Romita <romitafabio@gmail.com>
2018-03-10 13:01:39 +0100

    Adding version for hotello:auth0-oauth

--- package.js
+++ package.js
@@ -6,12 +6,12 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.5');
-  
+
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('hotello:auth0-oauth');
+  api.use('hotello:auth0-oauth@1.0.0');
   api.imply('hotello:auth0-oauth');
 
   api.addFiles("auth0.js");
